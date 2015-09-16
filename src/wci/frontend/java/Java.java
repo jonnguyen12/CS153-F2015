d4d87b00e83eaaa66f6eb71e03bfe72a359e0325
kbjohnson <kbjohnson831@yahoo.com>
2015-09-15 20:19:52 -0700

    project framework

---
+++ src/wci/frontend/java/Java.java
@@ -0,0 +1,10 @@
+package wci.frontend.java;
+
+public class Java {
+	
+	public static void main(String args[]) {
+		//our java parser
+		
+	}
+
+}
---
+++ src/wci/frontend/java/Token.java
@@ -0,0 +1,5 @@
+package wci.frontend.java;
+
+public class Token {
+	//re use token class from book/lecture?
+}
