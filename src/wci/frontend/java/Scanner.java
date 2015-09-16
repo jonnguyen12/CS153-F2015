2d53e7f1c6c6ab9e6d2c502daae35e5edcb1e867
Phuc Nguyen <stylephuc@gmail.com>
2015-09-16 14:49:03 -0700

    Move Kevin's src to Assignment 2

--- src/wci/frontend/java/Scanner.java
+++
@@ -1,5 +0,0 @@
-package wci.frontend.java;
-
-public class Scanner {
-	//reuse scanner class from lecture/book?
-}
--- src/wci/frontend/java/Token.java
+++
@@ -1,5 +0,0 @@
-package wci.frontend.java;
-
-public class Token {
-	//re use token class from book/lecture?
-}
